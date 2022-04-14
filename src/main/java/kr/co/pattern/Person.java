5df33bb666369e477c01a1160df7cf7b1c80d154
ksh030506 <llmm030506@naver.com>
2022-04-14 20:45:11 +0900

    lombok 추가

--- src/main/java/kr/co/pattern/Person.java
+++ src/main/java/kr/co/pattern/Person.java
@@ -1,28 +1,11 @@
 package kr.co.pattern;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Person {
     private String name;
-
     private int age;
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 }
